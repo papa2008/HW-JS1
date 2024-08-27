1423ece31eb4275da14e77dfdcd364b6f6a6dec6
papa2008 <112895777+papa2008@users.noreply.github.com>
2024-08-27 13:16:32 +0200

    HW1

---
+++ index.js
@@ -0,0 +1,36 @@
+// 1. 
+let userAge = 12;
+console.log(userAge);
+//2. 
+const userNickName = "Alex"
+console.log(userNickName);
+
+//3. 
+let isStudent = true;
+console.log(isStudent);
+//4. 
+let myString = "Hello, world"
+console.log(myString);
+//5. 
+let myNumber = 20;
+myNumber = myNumber + 10;
+console.log(myNumber);
+//6. 
+let myNull = null;
+console.log(myNull);
+//7. 
+let userFirstName = prompt("Enter your name:");
+console.log(userFirstName);
+let nameMesage = `Helllo hello ${userFirstName}, welcome to my website.`;
+alert(nameMesage);
+//8. 
+let Thanks = "Thanks";
+let userConfirm = confirm("Would you like to register");
+alert(Thanks);
+//9. 
+
+
+
+
+
+
